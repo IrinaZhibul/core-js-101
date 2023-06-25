0174a9deeacd8bd11632814e5d62957c4adbadaf
irinazhibul <zhibulirina@gmail.com>
2023-06-26 00:05:33 +0100

    fix: add name for function

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -25,7 +25,7 @@ function Rectangle(width, height) {
   this.height = height;
 }
 
-Rectangle.prototype.getArea = function () {
+Rectangle.prototype.getArea = function func() {
   return this.width * this.height;
 };
 
